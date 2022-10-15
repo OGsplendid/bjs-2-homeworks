055c333e7e4b5c557eefd59211f731dd71cbd192
OGsplendid <108668584+OGsplendid@users.noreply.github.com>
2022-10-15 19:00:46 +0300

    Add files via upload

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,26 @@
+'use strict';
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  let discriminant = b**2-4*a*c;
+  if (discriminant < 0) {
+  	arr.push('');
+  } else if (discriminant === 0) {
+  		arr.push(-b/(2*a));
+  } else if (discriminant > 0) {
+  		arr.push(-b + Math.sqrt(d) )/(2*a);
+  		arr.push(-b - Math.sqrt(d) )/(2*a);
+  }
   return arr; // array
 }
 
+solveEquation (1, 2, 3);
+
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
-
-  // код для задачи №2 писать здесь
+  let mainDebt = amount - contribution;
+  let creditTerm = 
 
   return totalAmount;
 }
