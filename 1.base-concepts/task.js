06794c805af8bab8e0b06cef292c979f555883df
OGsplendid <108668584+OGsplendid@users.noreply.github.com>
2022-10-16 13:48:02 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -8,8 +8,8 @@ function solveEquation(a, b, c) {
   } else if (discriminant === 0) {
   		arr.push(-b/(2*a));
   } else if (discriminant > 0) {
-  		arr.push(-b + Math.sqrt(d) )/(2*a);
-  		arr.push(-b - Math.sqrt(d) )/(2*a);
+  		arr.push(-b + Math.sqrt(d))/(2*a);
+  		arr.push(-b - Math.sqrt(d))/(2*a);
   }
   return arr; // array
 }
