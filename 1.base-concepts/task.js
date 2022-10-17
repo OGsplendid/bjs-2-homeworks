7a8fbdb9e4317159d8a5b01ddca790d460c09af8
OGsplendid <108668584+OGsplendid@users.noreply.github.com>
2022-10-17 20:50:02 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -8,8 +8,8 @@ function solveEquation(a, b, c) {
   } else if (discriminant === 0) {
   		arr.push(-b/(2*a));
   } else if (discriminant > 0) {
-  		arr.push(-b + Math.sqrt(discriminant)/(2*a));
-  		arr.push(-b - Math.sqrt(discriminant)/(2*a));
+  		arr.push((-b + Math.sqrt(discriminant))/(2*a));
+  		arr.push((-b - Math.sqrt(discriminant))/(2*a));
   }
   return arr;
 }
@@ -24,7 +24,7 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   let yearDiff = date.getFullYear() - currentYear;
   let termInMonths = yearDiff * 12 - currentMonth + date.getMonth();
   let monthlyPay = mainDebt * (percent/1200 + (percent/1200 / (((1 + percent/1200)**termInMonths) - 1)));
-  totalAmount = (monthlyPay * termInMonths).toFixed(2);
+  totalAmount = +(monthlyPay * termInMonths).toFixed(2);
   console.log(totalAmount);
 
   return totalAmount;
