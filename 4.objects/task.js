08d1a87b925dce8abf78094e78a6bfe40cb6fd6f
OGsplendid <108668584+OGsplendid@users.noreply.github.com>
2022-10-21 10:14:57 +0300

    Update task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,53 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+let student1 = new Student('Pedro', 'male', 19);
+let student2 = new Student('Maria', 'female', 21);
+let student3 = new Student('Jorje', 'male', 17);
+let student4 = new Student('Enrique', 'male', 24);
+
+
+
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function(mark) {
+	if (this.marks === undefined) {
+		this.marks = [];
+		this.marks.push(mark);
+	} else {
+		this.marks.push(mark);
+	}
+}
+
+Student.prototype.addMarks = function(...someMarks) {
+	if (this.marks === undefined) {
+		this.marks = [];
+		this.marks.push(someMarks);
+	} else {
+		this.marks.push(someMarks);
+	}
+}
+
+Student.prototype.getAverage = function() {
+	let avg;
+	let sum;
+	if (this.marks === undefined) {
+		return false;
+	} else {
+		this.marks.reduce((acc, mark) => {
+			sum = acc += mark;
+		}, 0);
+	}
+	avg = sum / this.marks.length;
+}
+
+Student.prototype.exclude = function(reason) {
+	this.excluded = reason;
+	delete this.subject;
+	delete this.marks;
+}
