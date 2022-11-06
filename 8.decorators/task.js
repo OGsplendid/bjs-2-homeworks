583c3964c3b4e3847c7bd9567e54fb8416b87189
OGsplendid <108668584+OGsplendid@users.noreply.github.com>
2022-11-06 14:55:08 +0300

    Update task.js

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,48 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  function wrapper(...args) {
+  	let hash = args.join(',');
+  	// let transitArr = [];
+  	// transitArr.push(hash);
+  	// transitArr.sort();
+  	// hash = transitArr.toString;
+  	let alreadySearched = cache.find(el => el.hash === hash);
+  	if (alreadySearched) {
+  		console.log("Из кэша: " + alreadySearched.value);
+  		return "Из кэша: " + alreadySearched.value;
+  	}
+
+  	const result = func(...args);
+  	cache.push({hash, value: result});
+
+  	if (cache.length > 5) {
+  		cache.shift();
+  	}
+  	console.log("Вычисляем: " + result);
+  	return "Вычисляем: " + result;
+  }
+  return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, delay) {
+	let timeoutId = 'first call';
+	function wrapper(...args) {
+		let allCount = 0;
+		allCount++;
+		let count = 0;
+		if (timeoutId === 'first call') {
+			return func(...args);
+			timeoutId = null;
+		}
+		if (timeoutId) {
+			clearTimout(timeoutId);
+		}
+		timoutId = setTimout(() => {
+			timeoutId = null;
+			count++;
+			return func(...args)
+		}, delay);
+	}
+	return wrapper;
 }
